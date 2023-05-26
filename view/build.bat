e3782d2d332201585f6339ac278783bcb9360660
David C W Brown <dave.github@brownsmeet.com>
2023-05-26 14:33:49 +0100

    Switch view to use Build.

--- view/build.bat
+++
@@ -1,7 +0,0 @@
-@rd /s /q build
-@md build >nul
-@..\source\ORB /b build TestWindows
-@if errorlevel 1 goto end
-@
-@build\TestWindows
-:end
--- view/buildoberon.bat
+++ view/buildoberon.bat
@@ -1,6 +1,6 @@
 @rd /s /q build
 @md build >nul
-@..\source\ORB /v /b build TestOberon
+@..\source\Build /v /b build TestOberon
 @if errorlevel 1 goto end
 @
 @build\TestOberon
