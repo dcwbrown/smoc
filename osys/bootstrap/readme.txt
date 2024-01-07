d41b1d74dee446a7c3cdc0c838371b69ec2a4614
David C W Brown <dave.github@brownsmeet.com>
2024-01-07 18:13:12 +0000

    Move files

---
+++ osys/bootstrap/readme.txt
@@ -0,0 +1,6 @@
+bootstrap - files to build the initial oberon compiler for osys
+
+Uses the modified patchouli compiler from smoc/source to build the
+osys Oberon compiler using source from this directory (stubs for
+Display, Fonts and Oberon), the osys directory (OR*.mod etc.), and
+the smoc source directory (boot, heap, kernel etc.)
--- osys/build.cmd
+++ osys/build.cmd
@@ -1,12 +1,11 @@
+@rem Build bootstrap compiler
+@cd bootstrap
 @mkdir build >NUL 2>NUL
-@copy ..\source\Boot.mod   . >NUL
-@copy ..\source\Kernel.mod . >NUL
-@copy ..\source\Files.mod  . >NUL
 @del *.smb >NUL
 @del *.code >NUL
-@..\source\Build /v /b build /s . OC2
-@if errorlevel 1 goto end
-@build\OC2
-@if errorlevel 1 goto end
-@start.exe
-:end
+@..\..\source\Build /v /b build /s ./;.. OC2
+@rem @if errorlevel 1 goto end
+@rem @build\OC2
+@rem @if errorlevel 1 goto end
+@rem @start.exe
+@rem :end
